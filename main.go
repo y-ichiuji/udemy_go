2d8a4d4db47d3a6a148431bd8a80a61be3c33f49
y-ichiuji <y-ichiuji@safie.jp>
2025-04-13 17:01:42 +0900

    feature: htmlの表示

--- main.go
+++ main.go
@@ -3,56 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/y-ichiuji/udemy-go/app/controllers"
 	"github.com/y-ichiuji/udemy-go/app/models"
 )
 
 func main() {
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
-	// u.Create()
-
-	// user, _ := models.GetUser(1)
-	// fmt.Println(user)
-
-	// user.Name = "test2"
-	// user.Email = "test2@example.com"
-	// user.Update()
-
-	// user, _ = models.GetUser(1)
-	// fmt.Println(user)
-
-	// user.Delete()
-	// user, _ = models.GetUser(1)
-	// fmt.Println(user)
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("First Todo")
-
-	// todo, _ := models.GetTodo(1)
-	// fmt.Println(todo)
-
-	// user3, _ := models.GetUser(3)
-	// user3.CreateTodo("Third Todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, todo := range todos {
-	// 	fmt.Println(todo)
-	// }
-
-	// user2, _ := models.GetUser(3)
-	// todos2, _ := user2.GetTodosByUser()
-	// for _, todo := range todos2 {
-	// 	fmt.Println(todo)
-	// }
-
-	todo, _ := models.GetTodo(3)
-	fmt.Println(todo)
-	todo.Delete()
-	deletedTodo, _ := models.GetTodo(3)
-	fmt.Println(deletedTodo)
+	controllers.StartMainServer()
 }
